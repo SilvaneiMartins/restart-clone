1ea9df4117676678dad799a982dd3db546b811bd
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-09-15 20:04:45 -0400

    Initial Commit

---
+++ restart-clone/restart_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  restart_cloneApp.swift
+//  restart-clone
+//
+//  Created by Silvanei  Martins on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct restart_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
